8a872f995970188c0106e45d3c6fc4df639b7d28
dadler <dadler@c71cb566-5a31-4692-9165-cf76bdc42cab>
2009-07-05 13:48:59 +0000

    added R memory allocation demo
    git-svn-id: http://dyncall.org/svn/dyncall/trunk@357 c71cb566-5a31-4692-9165-cf76bdc42cab

---
+++ bindings/R/rdyncall/demo/R_malloc.R
@@ -0,0 +1,12 @@
+# Dynport: Rmalloc
+# Description: R low-level memory management
+# Maintainer: dadler@uni-goettingen.de
+
+dynbind("R","R_chk_calloc(ii)p;R_chk_free(p)v;")
+malloc <- function(size) 
+{
+  x <- R_chk_calloc(as.integer(size),1L)
+  reg.finalizer(x, R_chk_free)
+  return(x)
+}
+
