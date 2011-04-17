dee38bc563900f2bb9f6933643f7811b6d030b3e
dadler <dadler@c71cb566-5a31-4692-9165-cf76bdc42cab>
2011-04-17 22:48:34 +0000

    added demo 'playtune' .. SDL audio/SDL_mixer mod playing demo
    git-svn-id: http://dyncall.org/svn/dyncall/trunk@988 c71cb566-5a31-4692-9165-cf76bdc42cab

---
+++ bindings/R/rdyncall/demo/playtune.R
@@ -0,0 +1,29 @@
+
+# Package: rdyncall 
+# File: demo/playtune.R
+# Description: play a nice oldsch00l tune.
+# Uses: SDL/audio, SDL_mixer
+
+rsrc <- function(name) system.file(paste("demo-files",name,sep=.Platform$file.sep), package="rdyncall")
+music <- NULL
+init <- function()
+{
+  require(rdyncall)
+  dynport(SDL)
+  SDL_Init(SDL_INIT_AUDIO)
+  dynport(SDL_mixer)
+  Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, 2, 4096)
+  music <<- Mix_LoadMUS(rsrc("external.xm"))
+}
+cleanup <- function()
+{
+  stopTune()
+  Mix_FreeMusic(music)
+  Mix_CloseAudio()
+  SDL_Quit()
+}
+
+playTune <- function() Mix_PlayMusic(music, 1) 
+pauseTune <- function() Mix_PauseMusic()
+stopTune <- function() Mix_HaltMusic()  
+
