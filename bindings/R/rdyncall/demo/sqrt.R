98282f4b73fbf26fbfefd40a830050e46ac305c0
dadler <dadler@c71cb566-5a31-4692-9165-cf76bdc42cab>
2011-07-20 13:15:12 +0000

    Minor example bugfix: Using math library from rdyncall requires to look for 'libm.so.6' as well on Debian 6 sid.
    git-svn-id: http://dyncall.org/svn/dyncall/trunk@1175 c71cb566-5a31-4692-9165-cf76bdc42cab

--- bindings/R/rdyncall/demo/sqrt.R
+++ bindings/R/rdyncall/demo/sqrt.R
@@ -2,7 +2,7 @@
 # File: demo/sqrt.R
 # Description: C math library demo (dynbind demo) 
 
-dynbind( c("msvcrt","m"), "sqrt(d)d;" )
+dynbind( c("msvcrt","m","m.so.6"), "sqrt(d)d;" )
 print(sqrt)
 sqrt(144)
 
